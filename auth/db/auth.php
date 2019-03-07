1b9b2da4d9272df27f54abe6937759829a712ac9
ealps2d@shinshu-u.ac.jp <ealps2d@shinshu-u.ac.jp>
2019-03-07 01:40:11 +0000

    外部データベース認証を SSHA 対応にする
    sha1 に SSHA 対応するコードを追加

--- auth/db/auth.php
+++ auth/db/auth.php
@@ -135,7 +135,12 @@ class auth_plugin_db extends auth_plugin_base {
             } else if ($this->config->passtype === 'md5') {
                 return (strtolower($fromdb) == md5($extpassword));
             } else if ($this->config->passtype === 'sha1') {
-                return (strtolower($fromdb) == sha1($extpassword));
+                if ( substr($fromdb,0,6) === '{SSHA}' ) {
+                    $salt = substr(base64_decode(substr($fromdb,6)),20);
+                    return ( $fromdb == ('{SSHA}' . base64_encode(sha1($extpassword . $salt, TRUE) . $salt)) );
+                } else {
+                    return (strtolower($fromdb) == sha1($extpassword));
+                }
             } else if ($this->config->passtype === 'saltedcrypt') {
                 return password_verify($extpassword, $fromdb);
             } else {
