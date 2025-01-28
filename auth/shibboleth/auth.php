1c1699d4c6770bdff9484e9ae1f030fa63447c6c
warnax-corp <kosuke@warnax.co.jp>
2025-01-28 14:10:53 +0900

    [共通] 氏名制御の参照元を変更
    * 姓の参照先も変更

--- auth/shibboleth/auth.php
+++ auth/shibboleth/auth.php
@@ -132,10 +132,9 @@ class auth_plugin_shibboleth extends auth_plugin_base {
                 if ($_SERVER['eduPersonAffiliation'] == 'student') {
                     $result[$key] = strtoupper($this->get_first_string($_SERVER['uid']));
                 } else {
-                    $userFullName = mb_convert_kana($this->get_first_string($_SERVER['jaGivenName']), 's') ;
+                    $userFullName = mb_convert_kana($this->get_first_string($_SERVER['jasn']), 's') ;
                     $userFullNameArray = explode(' ',$userFullName);
                     $result[$key] = $userFullNameArray[0];
-                    $result[$key] = $userFullName;
                 }
             // set firstname value from shibboleth
             } else if ($key == 'firstname'){
