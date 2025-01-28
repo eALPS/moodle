d0be2163718e119cbc2773912ca970a67fa9d54b
warnax-corp <kosuke@warnax.co.jp>
2025-01-28 13:51:53 +0900

    [共通] 自己署名証明書を許容するオプションを追加
    * PHP5.6 以降でピア証明書必須を回避するために postSend にオプションを追加

--- lib/phpmailer/moodle_phpmailer.php
+++ lib/phpmailer/moodle_phpmailer.php
@@ -142,6 +142,13 @@ class moodle_phpmailer extends \PHPMailer\PHPMailer\PHPMailer {
             phpunit_util::phpmailer_sent($mail);
             return true;
         } else {
+            $this->SMTPOptions = [
+                'ssl' => [
+                    'verify_peer' => false,
+                    'verify_peer_name' => false,
+                    'allow_self_signed' => true
+                ]
+            ];
             return parent::postSend();
         }
     }
